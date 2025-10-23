278f78bf91260c820b537931f21551e56c7d2894
king ali <hay19732000@outlook.com>
2025-10-23 16:09:05 +0300

    add all NuGet package. we need and add User and Role Model

--- Core/Models/Role.cs
+++ Core/Models/Role.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace AliHaydarBase.Api.Core.Models
 {
-    public class Role
+    public class Role : IdentityRole
     {
          public string? Description { get; set; }
     }
--- Core/Models/Users.cs
+++ Core/Models/Users.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace AliHaydarBase.Api.Core.Models
 {
-    public class User
+    public class User : IdentityUser
     {
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
